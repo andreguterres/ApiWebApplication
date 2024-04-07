3aca40bf9693a95c22a93243d665a5e3b4ef44c2
andre.guterres <andre.lepesteur@gmail.com>
2024-04-07 11:19:30 -0300

    Inicio implementação Controller Adicionar

--- WebApi/Controllers/ClienteController.cs
+++ WebApi/Controllers/ClienteController.cs
@@ -1,12 +1,46 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using WebApi.Entities;
+using WebApi.Repository;
 
 namespace WebApi.Controllers
 {
+    [Route("api/[Controller]")]
+    [ApiController]
     public class ClienteController : ControllerBase
     {
-        //public IActionResult Index()
+        private readonly ICliente _clientes;
+        //private readonly IMapper _mapper;
+
+
+        public ClienteController(ICliente clienteRepositorio/*, IMapper mapper*/)
+        {
+            _clientes = clienteRepositorio;
+            //_mapper = mapper;
+
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<List<Cliente>>> Adicionar(Cliente cliente)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            //var pedidoDTO = _mapper.Map<Pedido>(pedido);
+
+            await _clientes.Adicionar(cliente);
+
+            return Ok("Cliente criado com sucesso!");
+
+        }
+
+        //[HttpGet]
+
+        //public async Task<ActionResult<IEnumerable<Pedido>>> Pesquisar()
         //{
-        //    return View();
+        //    List<Pedido> pedido = await _pedidos.Pesquisar();
+
+        //    return Ok(pedido);
+
         //}
     }
 }
